559860ce1e8fdfe6e40535dcf43ddf7281a314b6
AbsHp <asinghal21@iitk.ac.in>
2024-05-05 18:34:13 +0530

    completed code

--- Dice Roller.py
+++ Dice Roller.py
@@ -3,5 +3,9 @@ import random
 while True:
     pass
 
-print(f"The value is", random.randint(1, 6))
+    print ("Rolling Dice...")
+    print(f"The value is", random.randint(1, 6))
 
+    repeat = input("Roll dice again? 'y' for yes and 'n' for no: ")
+    if repeat == 'n':
+        break
