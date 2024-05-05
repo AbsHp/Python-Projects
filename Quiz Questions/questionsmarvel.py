526fafa903ceada9cc59d8f90bbfb24115acd7d7
AbsHp <asinghal21@iitk.ac.in>
2024-05-05 20:12:36 +0530

    added qmarvel

---
+++ Quiz Questions/questionsmarvel.py
@@ -0,0 +1,26 @@
+quiz = {
+    1 : {
+        "question" : "Who is Iron Man?"
+        "answer" : "Tony Stark"
+    },
+    2 : {
+        "question" : "Who is the god of lightning in Avengers?"
+        "answer" : "Thor"
+    },
+    3 : {
+        "question" : "Who carries a shield of American flag theme in Avengers?",
+        "answer" : "Captain America"
+    },
+    4 : {
+        "question" : "Which avenger is green in color?",
+        "answer" : "Hulk"
+    },
+    5 : {
+        "question" : "Which avenger can change it's size?",
+        "answer" : "AntMan"
+    },
+    6 : {
+        "question" : "Which Avenger is red in color and has mind stone?",
+        "answer" : "Vision"
+    }
+}
