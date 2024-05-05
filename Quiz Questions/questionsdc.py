0527cca2b11433e155196d538f2c3420facd90f0
AbsHp <asinghal21@iitk.ac.in>
2024-05-05 20:18:12 +0530

    added qdc

---
+++ Quiz Questions/questionsdc.py
@@ -0,0 +1,26 @@
+quiz = {
+    1 : {
+        "question" : "Who is Batman?",
+        "answer" : "Bruce Wayne"
+    },
+    2 : {
+        "question" : "Who played Joker in 'Dark Knight'?",
+        "answer" : "Heath Ledger"
+    },
+    3 : {
+        "question" : "What is Superman's weakness?",
+        "answer" : "Kryptonite"
+    },
+    4 : {
+        "question" : "What is the name of Batman's butler?",
+        "answer" : "Alfred Pennyworth"
+    },
+    5 : {
+        "question" : "Where was Wonder Woman raised?",
+        "answer" : "Paradise Island (Themyscira)"
+    },
+    6 : {
+        "question" : "What is the name of the original Flash?",
+        "answer" : "Bartholomew Henry 'Barry' Allen"
+    }
+}
