29660c453b761e9289fe97e833350f23cb0630f8
AbsHp <asinghal21@iitk.ac.in>
2024-05-05 10:13:05 +0530

    added DC SSG

--- Random Short Story Generators/Random DC Short Story Generator.py
+++ Random Short Story Generators/Random DC Short Story Generator.py
@@ -1,5 +1,8 @@
 import random
 
 when = ['A long time ago', 'Once upon a time', 'Yesterday', 'Before you were born', 'In future']
-who = ['Shazam', 'Batman', 'Superman', 'Joker', ]
-went = []
+who = ['Shazam', 'Batman', 'Superman', 'Joker', 'Wonder Woman', 'The Flash']
+went = ['Arkham Asylum', 'Bat Cave', 'Gotham City', 'Manhattan', 'Atlantis', 'Paradise Island', 'Krypton']
+what = ['to eat a lot of cakes', 'to find peace', 'to fight for justice', 'to steal icecream', 'to dance', 'to practice archery']
+
+print(random.choice(when) + ', ' + random.choice(who) + ' went to ' + random.choice(went) + ' ' + random.choice(what) + '.')
