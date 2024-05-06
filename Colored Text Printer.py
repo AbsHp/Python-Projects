f90d27ccc3de2648523c214288c2a914db7afacc
AbsHp <asinghal21@iitk.ac.in>
2024-05-06 16:26:27 +0530

    code changes

--- Colored Text Printer.py
+++ Colored Text Printer.py
@@ -1,1 +1,19 @@
 import colorama
+from colorama import Back, Fore
+
+colorama.init(autoreset = True)
+
+text = input("Enter a phrase or sentence: ")
+
+print(Fore.RED + text)
+print(Back.RED + text)
+
+print(Fore.YELLOW + Back.MAGENTA  + text)
+print(Fore.GREEN + Back.WHITE  + text)
+print(Fore.CYAN + Back.BLUE  + text)
+print(Fore.GREEN + Back.MAGENTA  + text)
+print(Fore.RED + Back.YELLOW  + text)
+print(Fore.BLACK + Back.RED  + text)
+print(Fore.WHITE + Back.CYAN  + text)
+print(Fore.BLUE + Back.YELLOW  + text)
+print(Fore.MAGENTA + Back.RED  + text)
