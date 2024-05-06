c585e928268c1e84573377198d8726b841e9abfc
AbsHp <asinghal21@iitk.ac.in>
2024-05-06 18:04:41 +0530

    added the code

--- Madlib Game.py
+++ Madlib Game.py
@@ -1,3 +1,5 @@
 noun = input("Noun: ")
 adj = input("Adjective: ")
-noun2 = input("Noun:")
+noun2 = input("Noun: ")
+
+print(f"After hiding the painting in his {noun} for two years, he grew {adj} and tried to sell it to a {noun2} in Mumbai, but was caught...")
