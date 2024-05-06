0a6baf05676369d9ac02aa92e0a83289230d662e
AbsHp <asinghal21@iitk.ac.in>
2024-05-06 18:00:43 +0530

    added the code

--- Guess the Number Game.py
+++ Guess the Number Game.py
@@ -2,5 +2,15 @@ import random
 
 max_num = 30
 
-random_number = random.rnadint(1, max_num)
+random_number = random.randint(1, max_num)
 
+guess = 0
+
+while guess != random_number:
+    guess = int(input(f"Enter a number between 1 and {max_num}: "))
+    if guess < random_number:
+        print("Wrong! Too low...")
+    elif guess > random_number:
+        print("Wrong! Too high...")
+
+print(f"You guessed it! The number indeed was {random_number}.")
